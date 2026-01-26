018ff54949087a15441ba4433ce0d7b81b882f4e
Arrov47 <ar.rov47@gmail.com>
2026-01-26 22:01:48 +0500

    Dependency injection was added

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/bloc/themeBloc/theme_bloc.dart';
+import 'core/bloc/themeBloc/theme_state.dart';
+import 'core/serviceLocator/service_locator.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MyApp(),));
+  runApp(const Home());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Hello World!"),
+    return BlocProvider(
+      create: (_) => sl<ThemeBloc>(),
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            theme: state.themeData,
+            home: Scaffold(
+              appBar: AppBar(
+                title: Text("Registration page"),
+                centerTitle: true,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
