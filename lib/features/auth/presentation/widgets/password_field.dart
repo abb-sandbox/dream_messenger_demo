0aa213d49e09ce152fa75adc59e1ca72dd42b9a1
Arrov47 <ar.rov47@gmail.com>
2026-01-27 22:51:03 +0500

    Added login,password fields and auth google for SignUp page

---
+++ lib/features/auth/presentation/widgets/password_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({super.key});
+
+  @override
+  State<PasswordField> createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool passwordObscured = true;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      // height: size.height * (1 / 16),
+      child: TextField(
+        obscureText: passwordObscured,
+        style: TextStyle(color: theme.colorScheme.surface),
+        cursorColor: theme.colorScheme.surface,
+        decoration: InputDecoration(
+          suffixIcon: IconButton(
+            onPressed: () => setState(() {
+              passwordObscured = !passwordObscured;
+            }),
+            icon: Icon(
+              passwordObscured ? Icons.visibility_off : Icons.visibility,
+            ),
+          ),
+          hintText: "Password",
+          hintStyle: TextStyle(
+            fontStyle: FontStyle.italic,
+            color: theme.colorScheme.surface.withOpacity(0.8),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: theme.inputDecorationTheme.border!.borderSide.color,
+            ),
+            borderRadius: BorderRadius.circular(size.width * 0.01),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: theme.inputDecorationTheme.border!.borderSide.color,
+            ),
+            borderRadius: BorderRadius.circular(size.width * 0.01),
+          ),
+        ),
+      ),
+    );
+  }
+}
