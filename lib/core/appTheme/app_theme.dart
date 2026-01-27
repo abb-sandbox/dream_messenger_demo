6d4afa31e80fba9ef837e8b250683dcff8db9831
Arrov47 <ar.rov47@gmail.com>
2026-01-27 22:51:03 +0500

    Added login,password fields and auth google for SignUp page

--- lib/core/appTheme/app_theme.dart
+++ lib/core/appTheme/app_theme.dart
@@ -2,31 +2,60 @@ import 'package:flutter/material.dart';
 
 abstract class AppTheme {
   static final lightTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.grey.shade300,
+    scaffoldBackgroundColor: Colors.white,
+    iconTheme: IconThemeData(color: Colors.black),
     iconButtonTheme: IconButtonThemeData(
       style: ButtonStyle(
-        iconColor: WidgetStateProperty.all(Colors.grey.shade800),
+        overlayColor: WidgetStateProperty.all(
+          Colors.black.withOpacity(0.4),
+        ),
+        iconColor: WidgetStateProperty.all(Colors.black),
       ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+      // enabledBorder: OutlineInputBorder(),
+      // focusedBorder: OutlineInputBorder(),
+      // outlineBorder: BorderSide(color: Colors.grey.shade800),
+      // focusedErrorBorder: OutlineInputBorder(
+      //   borderSide: BorderSide(color: Colors.red.shade700),
+      // ),
     ),
     colorScheme: ColorScheme(
       brightness: Brightness.light,
       primary: Colors.blue.shade500,
-      onPrimary: Colors.grey.shade300,
+      onPrimary: Colors.white,
       secondary: Colors.indigoAccent.shade400,
       onSecondary: Colors.indigoAccent.shade200,
       error: Colors.red.shade600,
       onError: Colors.red.shade400,
-      surface: Colors.teal.shade500,
-      onSurface: Colors.teal.shade300,
+      surface: Colors.black,
+      onSurface: Colors.white,
     ),
   );
 
+  //-------------------------------------------------------------------------------------------------------------------
+
   static final darkTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.grey.shade800,
+    scaffoldBackgroundColor: Colors.black,
+    iconTheme: IconThemeData(color: Colors.white),
     iconButtonTheme: IconButtonThemeData(
       style: ButtonStyle(
-        iconColor: WidgetStateProperty.all(Colors.grey.shade300),
+        overlayColor: WidgetStateProperty.all(
+          Colors.white.withOpacity(0.4),
+        ),
+        iconColor: WidgetStateProperty.all(Colors.white),
       ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      // outlineBorder: ,
+      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+      // enabledBorder: OutlineInputBorder(),
+      // focusedBorder: OutlineInputBorder(),
+      // outlineBorder: BorderSide(color: Colors.grey.shade300),
+      // focusedErrorBorder: OutlineInputBorder(
+      //   borderSide: BorderSide(color: Colors.red.shade800),
+      // ),
     ),
     colorScheme: ColorScheme(
       brightness: Brightness.dark,
@@ -36,8 +65,8 @@ abstract class AppTheme {
       onSecondary: Colors.indigoAccent,
       error: Colors.red.shade800,
       onError: Colors.red.shade600,
-      surface: Colors.teal.shade700,
-      onSurface: Colors.teal.shade500,
+      surface: Colors.white,
+      onSurface: Colors.black,
     ),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dream_messenger_demo/features/auth/presentation/pages/sign_up_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/bloc/themeBloc/theme_bloc.dart';
@@ -21,10 +22,22 @@ class Home extends StatelessWidget {
       create: (_) => sl<ThemeBloc>(),
       child: BlocBuilder<ThemeBloc, ThemeState>(
         builder: (context, state) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: state.themeData,
-            home: SignUpPage(state: state),
+          return AnnotatedRegion<SystemUiOverlayStyle>(
+            value: state.themeData.brightness == Brightness.dark
+                ? SystemUiOverlayStyle.light
+                : SystemUiOverlayStyle.dark,
+            child: MaterialApp(
+              builder: (context, child) {
+                return GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+                  child: child,
+                );
+              },
+              debugShowCheckedModeBanner: false,
+              theme: state.themeData,
+              home: SignUpPage(),
+            ),
           );
         },
       ),
