e94c81003d2c6e64740b6355ecdc83710da868f0
Arrov47 <ar.rov47@gmail.com>
2026-02-03 17:06:33 +0500

    SignIn page was created, and auth init is oraganized in a structured way

--- lib/core/dependencyInjection/service_locator.dart
+++ lib/core/dependencyInjection/service_locator.dart
@@ -1,10 +1,11 @@
 import 'package:dream_messenger_demo/core/bloc/themeBloc/theme_bloc.dart';
-import 'package:dream_messenger_demo/core/serviceLocator/auth_dependencies.dart';
 import 'package:dream_messenger_demo/core/services/local_data_service.dart';
 import 'package:dream_messenger_demo/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'auth_dependencies.dart';
 
 final sl = GetIt.instance;
 
