c262d99921d84bd214760dfee0a709ec2ecf66c9
Arrov47 <ar.rov47@gmail.com>
2026-03-11 01:13:59 +0500

    feat(auth): sign up with firebase

--- lib/features/auth/auth_dependencies.dart
+++ lib/features/auth/auth_dependencies.dart
@@ -3,8 +3,10 @@ import 'package:dream_messenger_demo/core/dependencyInjection/service_locator.da
 import 'package:dream_messenger_demo/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:dream_messenger_demo/features/auth/domain/repositories/auth_repository.dart';
 import 'package:dream_messenger_demo/features/auth/domain/usecases/sign_in_usecase.dart';
+import 'package:dream_messenger_demo/features/auth/domain/usecases/sign_up_usecase.dart';
 import 'package:dream_messenger_demo/features/auth/domain/usecases/verify_email_usecase.dart';
 import 'package:dream_messenger_demo/features/auth/presentation/bloc/signInBloc/sign_in_bloc.dart';
+import 'package:dream_messenger_demo/features/auth/presentation/bloc/signUpBloc/sign_up_bloc.dart';
 import 'package:dream_messenger_demo/features/auth/presentation/bloc/verifyEmailBloc/verify_email_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -26,6 +28,8 @@ Future<void> initAuthDependencies() async {
 
   sl.registerSingleton(VerifyEmailUseCase(authRepository: sl()));
 
+  sl.registerSingleton(SignUpUseCase(authRepository: sl()));
+
   sl.registerSingleton(SignInUseCase(authRepository: sl()));
 
   sl.registerFactory<VerifyEmailBloc>(
@@ -33,4 +37,6 @@ Future<void> initAuthDependencies() async {
   );
 
   sl.registerFactory<SignInBloc>(() => SignInBloc(signInUseCase: sl()));
+
+  sl.registerFactory<SignUpBloc>(() => SignUpBloc(signUpUseCase: sl()));
 }
