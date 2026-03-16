5c6d313c7f723a3e0d13c3befed8d027533e6e63
Arrov47 <ar.rov47@gmail.com>
2026-03-17 01:32:47 +0500

    feat(auth): implement a logic of checking user auth state

--- lib/core/services/auth_service.dart
+++ lib/core/services/auth_service.dart
@@ -6,11 +6,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   bool? _userSignedIn;
+  String? _userEmail;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Stream<User?> get userStatus => _auth.authStateChanges();
 
   Future<void> get signOut => _auth.signOut();
+
+  String? get userEmail => _userEmail;
 
   Future<bool> isUserSignedIn() async {
     if (_userSignedIn != null) return _userSignedIn!;
@@ -19,6 +22,7 @@ class AuthService {
       Constants.emailKey,
     );
     if (result != null && result.isNotEmpty) {
+      _userEmail = result;
       _userSignedIn = true;
       return _userSignedIn!;
     }
