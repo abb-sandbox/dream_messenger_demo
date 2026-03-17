d0c318b59b32d633b8b2659670e441ae87bc5064
Arrov47 <ar.rov47@gmail.com>
2026-03-17 19:12:35 +0500

    feat(chatList): create side navigation bar

--- lib/features/chat/presentation/pages/chat_list_page.dart
+++ lib/features/chat/presentation/pages/chat_list_page.dart
@@ -1,12 +1,15 @@
 import 'package:dream_messenger_demo/core/utils/responsive_helper.dart';
 import 'package:dream_messenger_demo/features/auth/presentation/widgets/screen_coverage.dart';
 import 'package:dream_messenger_demo/features/chat/presentation/widgets/chat_card.dart';
+import 'package:dream_messenger_demo/features/chat/presentation/widgets/navigation_side_bar.dart';
 import 'package:flutter/material.dart';
 
 class ChatListPage extends StatelessWidget {
   final String email;
 
-  const ChatListPage({super.key, required this.email});
+  ChatListPage({super.key, required this.email});
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +17,17 @@ class ChatListPage extends StatelessWidget {
     return ScreenCoverage(
       child: SafeArea(
         child: Scaffold(
+          key: _scaffoldKey,
           body: Column(
             children: [
               Row(
                 children: [
-                  IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                  IconButton(
+                    onPressed: () {
+                      _scaffoldKey.currentState?.openDrawer();
+                    },
+                    icon: Icon(Icons.menu),
+                  ),
                   Padding(
                     padding: EdgeInsets.only(
                       left: context.responsiveValue(
@@ -28,7 +37,7 @@ class ChatListPage extends StatelessWidget {
                       ),
                     ),
                     child: Text(
-                      email,
+                      "Dream",
                       style: TextStyle(
                         color: theme.colorScheme.onSurface,
                         fontSize: context.responsiveValue(
@@ -61,6 +70,7 @@ class ChatListPage extends StatelessWidget {
               ),
             ],
           ),
+          drawer: NavigationSideBar(),
         ),
       ),
     );
